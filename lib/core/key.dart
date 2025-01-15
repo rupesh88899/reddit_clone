72d364665f60b2caba1994316b491c5c21480399
RUPESH KUMAR TIWARI <116380411+rupesh88899@users.noreply.github.com>
2025-01-15 19:17:15 +0530

    Update key.dart
    here you can set your cloudName to use Cloudneary

--- lib/core/key.dart
+++ lib/core/key.dart
@@ -1,4 +1,4 @@
 // Add your cloud name and upload preset(unsined) here to use cloudinary for image upload
 
-const String cloudName = 'du9rdxgg7';
+const String cloudName = '*********';
 const String uploadPreset = 'cloudinary_reddit_clone';
